94e53c5a7a4756694842aaa4a4986bbe1e0d710c
maggun <umi.21@uni-dubna.ru>
2022-07-04 23:46:57 +0300

    Добавил классы Character и Weapon

---
+++ ConsoleVersion/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleVersion
+{
+    internal class Weapon
+    {
+        private string name;
+        private int damage;
+        private int consumptionOfActionPoints;
+        public string Name { get => name; }
+        public int Damage { get => damage; }
+        public int ConsumptionOfActionPoints { get => consumptionOfActionPoints; }
+    }
+}
