c9b3ebd064029a84c17afa57035363476429bd18
maggun <umi.21@uni-dubna.ru>
2022-07-05 17:57:59 +0300

    Редактирование

---
+++ WindowsFormsVersion/Character.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsVersion
+{
+    public class Character
+    {
+        private string name;
+        private int health;
+        private int armor;
+        private int maxHealth;
+        public string Name { get => name; set => name = value; }
+        public int Health { get => health; set => health = value; }
+        public int Armor
+        {
+            get => armor;
+            set
+            {
+                if (value == 0)
+                    armor = 1;
+            }
+        }
+        public Character(string name, int health, int armor)
+        {
+            this.name = name;
+            this.health = health;
+            this.armor = armor;
+            maxHealth = health;
+        }
+        public void Hit(Character enemy, Weapon weapon)
+        {
+            enemy.Health = enemy.Health - weapon.Damage * 1 / enemy.Armor;
+        }
+        public void Heal() => health = maxHealth;
+
+    }
+}
---
+++ WindowsFormsVersion/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsVersion
+{
+    public class Weapon
+    {
+        private string name;
+        private int damage;
+        public string Name { get => name; }
+        public int Damage { get => damage; }
+        public Weapon(string name, int damage)
+        {
+            this.name = name;
+            this.damage = damage;
+        }
+    }
+}
